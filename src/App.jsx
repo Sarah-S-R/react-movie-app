f7d5aa0396fd7925a86df3baf1b67b706dc47160
Sarah <143218168+Sarah-S-R@users.noreply.github.com>
2024-04-23 08:59:30 -0230

    created components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,28 @@
-import { useState } from 'react'
+import { useEffect } from 'react';
+import axios from 'axiox';
 
 import './App.css'
 
 function App() {
+
+  const getMovies = async () => {
+      try{
+        const data = await axios.get("https://movies-app.prakashsakari.repl.co/api/movies");
+        console.log (data);
+
+      }catch(err) {
+        console.log(err)
+      }
+  }
   
 
+  useEffect (() => {
+    getMovies();
+  }, [])
+
+  
   return (
+
     <div className= "App">
       Hello Geeks
       
